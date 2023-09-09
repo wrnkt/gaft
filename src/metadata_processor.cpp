eec552a5c62ea1caf6a9a6339d67e5994a3ac6e9
Niko Ware <wrnkt1@gmail.com>
2023-09-09 03:06:31 -0400

    Alias std::filesystem

--- src/metadata_processor.cpp
+++ src/metadata_processor.cpp
@@ -4,19 +4,19 @@
 #include <filesystem>
 #include <vector>
 
-using namespace std;
-
-path_preprocess_info_t metadata_processor_t::preprocess_info(const filesystem::path& p) {
+path_preprocess_info_t metadata_processor_t::preprocess_info(const fs::path& p) {
     return path_preprocess_info_t {
         .path_exists = path_exists(p),
         .is_dir = is_dir(p)
     };
 }
 
-bool metadata_processor_t::path_exists(const filesystem::path& p) {
-    return filesystem::exists(p);
+bool metadata_processor_t::path_exists(const fs::path& p) {
+    return fs::exists(p);
 }
 
-bool metadata_processor_t::is_dir(const filesystem::path& p) {
-    return filesystem::is_directory(p);
+bool metadata_processor_t::is_dir(const fs::path& p) {
+    return fs::is_directory(p);
+}
+
 }
--- src/metadata_processor.hpp
+++ src/metadata_processor.hpp
@@ -4,8 +4,10 @@
 #include <filesystem>
 #include <vector>
 
+namespace fs = std::filesystem;
+
 struct file_metadata_t {
-    std::filesystem::path file_path;
+    fs::path file_path;
     std::string file_name;
     uintmax_t file_size;
 };
@@ -20,13 +22,15 @@ struct path_preprocess_info_t {
 
 class metadata_processor_t {
     public:
-        path_preprocess_info_t preprocess_info(const std::filesystem::path& p);
-        std::vector<file_metadata_t> get_recursive_file_metadata(const std::filesystem::path& dir_path);
+        path_preprocess_info_t preprocess_info(const fs::path& p);
+        std::vector<file_metadata_t> get_recursive_file_metadata(const fs::path& dir_path);
 
     private:
-        uintmax_t compute_file_size(const std::filesystem::path& p);
-        bool get_metadata(const std::filesystem::path& file_path, file_metadata_t* const fm);
-        bool path_exists(const std::filesystem::path& p);
-        bool is_dir(const std::filesystem::path& p);
+        uintmax_t compute_file_size(const fs::path& p);
+
+        bool get_metadata(const fs::path& file_path, file_metadata_t* const fm);
+
+        bool path_exists(const fs::path& p);
+        bool is_dir(const fs::path& p);
 };
 
