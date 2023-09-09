9e535d86c58438a1eb577484b9426b2fe73f2725
Niko Ware <wrnkt1@gmail.com>
2023-09-09 09:45:13 -0400

    Debub flag

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++-13 -std=c++20 src/main.cpp ./src/metadata_processor.cpp -o ./bin/main -lsqlite3
+g++-13 -std=c++20 -ggdb src/main.cpp ./src/metadata_processor.cpp -o ./bin/main -lsqlite3
 exit 0
