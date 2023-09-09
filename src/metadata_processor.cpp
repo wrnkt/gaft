1e1d871e82dae90e4f14e77139e00596ec7beb67
Niko Ware <wrnkt1@gmail.com>
2023-09-09 03:43:35 -0400

    Notes

--- src/metadata_processor.cpp
+++ src/metadata_processor.cpp
@@ -11,6 +11,15 @@ path_preprocess_info_t metadata_processor_t::preprocess_info(const fs::path& p) 
     };
 }
 
+// TODO: not implemented
+// std::vector<file_metadata_t> get_recursive_file_metadata(const fs::path& dir_path);
+
+// TODO: not implemented
+// bool remove_file_filter(file_filter f);
+
+// TODO: not implemented
+// bool add_file_filter(file_filter f);
+
 bool metadata_processor_t::path_exists(const fs::path& p) {
     return fs::exists(p);
 }
@@ -19,4 +28,8 @@ bool metadata_processor_t::is_dir(const fs::path& p) {
     return fs::is_directory(p);
 }
 
-}
+// TODO: not implemented
+//bool metadata_processor_t::valid_file(const fs::path& f_path);
+
+// TODO: not implemented
+// bool metadata_processor_t::get_metadata(const fs::path& file_path, file_metadata_t* const fm);
