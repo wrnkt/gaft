ffef7ea603d6a488d257723dcf928498db1d40b7
Niko Ware <wrnkt1@gmail.com>
2023-09-09 10:22:15 -0400

    Fix naming

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
     std::string path = argv[1];
 
     metadata_processor_t proc;
-    proc.add_file_filter(file_ext_filter::TEXT);
+    proc.add_file_ext_filter(file_ext_filter::TEXT);
     path_preprocess_info_t info = proc.preprocess_info(path);
     std::cout << std::format("path_exists: {}\nis_dir: {}\nis_valid: {}", info.path_exists, info.is_dir, info.is_processable()) << std::endl;
     std::cout << std::format("valid file: {}\n", proc.valid_file_ext(path)) << std::endl;
