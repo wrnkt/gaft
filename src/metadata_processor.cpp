b1727389c34b34d638423570a6cd9f3f918eed12
Niko Ware <wrnkt1@gmail.com>
2023-09-09 02:34:43 -0400

    Rename preprocessing data and logic

--- src/metadata_processor.cpp
+++ src/metadata_processor.cpp
@@ -6,13 +6,11 @@
 
 using namespace std;
 
-
-dir_process_result_t metadata_processor_t::confirm_for_processing(const filesystem::path& p) {
-    dir_process_result_t result {
+path_preprocess_info_t metadata_processor_t::preprocess_info(const filesystem::path& p) {
+    return path_preprocess_info_t {
         .path_exists = path_exists(p),
         .is_dir = is_dir(p)
     };
-    return result;
 }
 
 bool metadata_processor_t::path_exists(const filesystem::path& p) {
--- src/metadata_processor.hpp
+++ src/metadata_processor.hpp
@@ -10,18 +10,19 @@ struct file_metadata_t {
     uintmax_t file_size;
 };
 
-struct dir_process_result_t {
+struct path_preprocess_info_t {
     bool path_exists { false };
     bool is_dir { false };
-    bool is_valid() {
+    bool is_processable() {
         return path_exists && is_dir;
     }
 };
 
 class metadata_processor_t {
     public:
+        path_preprocess_info_t preprocess_info(const std::filesystem::path& p);
         std::vector<file_metadata_t> get_recursive_file_metadata(const std::filesystem::path& dir_path);
-        dir_process_result_t confirm_for_processing(const std::filesystem::path& p);
+
     private:
         uintmax_t compute_file_size(const std::filesystem::path& p);
         bool get_metadata(const std::filesystem::path& file_path, file_metadata_t* const fm);
