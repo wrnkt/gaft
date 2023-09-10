b9f4b9c5957f57bedb50f559f1edb2d0e277610f
Niko Ware <wrnkt1@gmail.com>
2023-09-10 06:40:23 -0400

    Update build script for gui and console

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-g++-13 -std=c++20 -ggdb src/main.cpp ./src/metadata_processor.cpp -o ./bin/main -lsqlite3
+
+if [ "$1" == "console" ]
+    then
+    g++-13 -std=c++20 -ggdb \
+        ./src/console.cpp \
+        ./src/metadata_processor.cpp \
+        ./src/session.cpp \
+        -o ./bin/gaft-console -lsqlite3
+elif [ "$1" == "ui" ]
+then
+    g++-13 -std=c++20 -ggdb \
+        ./src/main.cpp \
+        ./src/metadata_processor.cpp \
+        -o ./bin/gaft -lsqlite3
+fi
 exit 0
