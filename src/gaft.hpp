741dcd041d491516798dcd3a314e2d52cc486d45
Niko Ware <wrnkt1@gmail.com>
2023-09-11 05:49:04 -0400

    Add test audio extensions

--- src/gaft.hpp
+++ src/gaft.hpp
@@ -7,14 +7,20 @@
 enum class GAFT_F_EXT {
     NONE,
     UNKNOWN,
+
     TXT,
-    MD
+    MD,
+
+    MP3,
+    MP4,
+    WAV
 };
 
 enum class GAFT_F_KIND : char {
     UNKNOWN,
     TEXT,
-    AUDIO
+    AUDIO,
+    VIDEO
 };
 
 typedef struct {
@@ -28,6 +34,11 @@ using extension_info_map = const std::unordered_map<std::string, gaft_f_info>;
 const extension_info_map FILE_CLASSIFICATION_MAP {
     { "", { GAFT_F_EXT::NONE, GAFT_F_KIND::UNKNOWN } },
     { ".", { GAFT_F_EXT::NONE, GAFT_F_KIND::UNKNOWN } },
+
     { ".txt", { GAFT_F_EXT::TXT, GAFT_F_KIND::TEXT } },
-    { ".md", { GAFT_F_EXT::MD, GAFT_F_KIND::TEXT } }
+    { ".md", { GAFT_F_EXT::MD, GAFT_F_KIND::TEXT } },
+
+    { ".mp3", { GAFT_F_EXT::MP3, GAFT_F_KIND::AUDIO } },
+    { ".mp4", { GAFT_F_EXT::MP4, GAFT_F_KIND::VIDEO } },
+    { ".wav", { GAFT_F_EXT::WAV, GAFT_F_KIND::AUDIO } }
 };
